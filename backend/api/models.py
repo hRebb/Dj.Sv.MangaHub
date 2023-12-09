47e783296194c8aa6b7cbffa2cbf41f5bd208144
hamza_rebb <hamza.rebb@gmail.com>
2023-12-09 17:12:25 +0100

    create db

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    genre = models.ManyToManyField(Genre)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    read = models.BooleanField(default=False)
