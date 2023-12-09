dc3ac81c742d18c7f6b444954a5b416d5aaa0ce2
hamza_rebb <hamza.rebb@gmail.com>
2023-12-09 17:34:58 +0100

    little update on the genre name

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from api.views import BookViewSet, GenreViewSet
+
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+router.register(r'genres', GenreViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
