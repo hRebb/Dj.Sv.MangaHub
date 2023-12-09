7a5dc7e872835ebe027cbc970a8e28bad93a15d6
hamza_rebb <hamza.rebb@gmail.com>
2023-12-09 17:29:03 +0100

    add api view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import BookSerializer, GenreSerializer
+from .models import Book, Genre
 
 # Create your views here.
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
