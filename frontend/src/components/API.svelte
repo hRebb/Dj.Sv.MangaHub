97765870e83c5d17d8fdd1786eb48de1f29c1312
hamza_rebb <hamza.rebb@gmail.com>
2023-12-10 13:29:13 +0100

    start working about front

--- frontend/src/components/API.svelte
+++ frontend/src/components/API.svelte
@@ -1,28 +1,78 @@
 <script lang="ts">
-    import { onMount } from'svelte';
+    import { onMount } from 'svelte';
 
-    type book = {
+    type Book = {
+        id: number,
         title: string,
         author: string,
-        rating: string
-    }
+        rating: string,
+        read: boolean,
+        genre: number[],
+        classification: number[],
+    };
 
-    let books: book[] = [];
+    type Genre = {
+        id: number,
+        name: string,
+    };
+
+    type Classification = {
+        id: number,
+        name: string,
+    };
+
+    let books: Book[] = [];
+    let genres: Genre[] = [];
+    let classifications: Classification[] = [];
 
     onMount(async () => {
-        const response = await fetch('http://127.0.0.1:8000/api/books/');
-        if (response.ok) {
-            books = await response.json();
-        } else {
-            console.log('Error:', response.status);
+        try {
+            const [booksResponse, genresResponse, classificationsResponse] = await Promise.all([
+                fetch('http://127.0.0.1:8000/api/books/'),
+                fetch('http://127.0.0.1:8000/api/genre/'),
+                fetch('http://127.0.0.1:8000/api/classifications/')
+            ]);
+
+            if (!booksResponse.ok || !genresResponse.ok || !classificationsResponse.ok) {
+                console.error('Error fetching data');
+                return;
+            }
+
+            books = await booksResponse.json();
+            genres = await genresResponse.json();
+            classifications = await classificationsResponse.json();
+        } catch (error) {
+            console.error('Error fetching data:', error);
         }
     });
 </script>
 
-<ul>
-    {#each books as book}
-        <li>{book.title}</li>
-        <li>{book.author}</li>
-        <li>{book.rating}</li>
-    {/each}
-</ul>
+{#await books}
+    <p>Loading...</p>
+{:then}
+    <ul>
+        {#each books as book}
+            <li>{book.title}</li>
+            <li>{book.author}</li>
+            <li>{book.rating}</li>
+            {#if book.read}
+                <li>Has been read</li>
+            {:else}
+                <li>Not Yet</li>
+            {/if}
+            <li>
+                Genres: 
+                    {#each book.genre as genreId}
+                        {genres.find(g => g.id === genreId)?.name}
+                    {/each}
+            </li>
+            <li>
+                Classification:
+                {#each book.classification as classificationId} 
+                    {classifications.find(c => c.id === classificationId)?.name}
+                {/each}
+            </li>
+            <br>
+        {/each}
+    </ul>
+{/await}
